1e205a60b61dac1750721ccd2b35d4f1c05ea51c
Daniel da Cruz Vieira <d260398@dac.unicamp.br>
2023-06-15 14:42:26 -0300

    Classe Bispo

---
+++ Bispo.java
@@ -0,0 +1,63 @@
+public class Bispo extends Peca{
+    public boolean moverPeca(String destino) {
+        // Implementação do movimento do Bispo
+        int deltaX = Math.abs(getPosX(this.getPosicao()) - getPosX(destino));
+        int deltaY = Math.abs(getPosY(this.getPosicao()) - getPosY(destino));
+
+        if(deltaX != deltaY) // Fora do alcance do Bispo
+            return false;
+
+        int nx = getPosX(destino);
+        int ny = getPosY(destino);
+        if(nx < 0 || nx >= 8 || ny < 0 || ny >= 8) // Posição do grid inválida
+            return false;
+        
+        if(nx > getPosX(this.getPosicao()) && ny > getPosY(this.getPosicao())) { // Movimento na diagonal superior direita
+            for(int i = 1; i < deltaX; i++) {
+                if(this.getTabuleiro().getPeca(getPosX(this.getPosicao()) + i, getPosY(this.getPosicao()) + i) != null) { // Caso haja alguma peça entre o Bispo e o destino
+                    return false;
+                }
+            }
+        }
+        else if(nx > getPosX(this.getPosicao()) && ny < getPosY(this.getPosicao())) { // Movimento na diagonal inferior direita
+            for(int i = 1; i < deltaX; i++) {
+                if(this.getTabuleiro().getPeca(getPosX(this.getPosicao()) + i, getPosY(this.getPosicao()) - i) != null) { // Caso haja alguma peça entre o Bispo e o destino
+                    return false;
+                }
+            }
+        }
+        else if(nx < getPosX(this.getPosicao()) && ny > getPosY(this.getPosicao())) { // Movimento na diagonal superior esquerda
+            for(int i = 1; i < deltaX; i++) {
+                if(this.getTabuleiro().getPeca(getPosX(this.getPosicao()) - i, getPosY(this.getPosicao()) + i) != null) { // Caso haja alguma peça entre o Bispo e o destino
+                    return false;
+                }
+            }
+        }
+        else if(nx < getPosX(this.getPosicao()) && ny < getPosY(this.getPosicao())) { // Movimento na diagonal inferior esquerda
+            for(int i = 1; i < deltaX; i++) {
+                if(this.getTabuleiro().getPeca(getPosX(this.getPosicao()) - i, getPosY(this.getPosicao()) - i) != null) { // Caso haja alguma peça entre o Bispo e o destino
+                    return false;
+                }
+            }
+        }
+
+        if(this.getTabuleiro().getPeca(nx, ny) != null) { // Caso a posição de destino não esteja vazia
+            if(!this.getTabuleiro().getPeca(nx, ny).getCorDono().equals(this.getCorDono())) { // Peça inimiga
+                return this._make_move(destino);
+            }
+            else
+                return false;
+        }
+        else { // Caso a posição de destino esteja vazia
+            return this._make_move(destino);
+        }
+    }
+
+    public Bispo(String corDono, Tabuleiro tabuleiro, String posicao){
+        super(corDono, tabuleiro, posicao);
+        if(corDono.equals("branca"))
+            setLabel("B");
+        else
+            setLabel("b");
+    }
+}
