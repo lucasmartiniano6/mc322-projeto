278de99beca1a9f2774ba128b997abc470130c80
Daniel da Cruz Vieira <d260398@dac.unicamp.br>
2023-06-15 14:37:22 -0300

    Classe Torre + Roque

--- Rei.java
+++ Rei.java
@@ -4,21 +4,60 @@ public class Rei extends Peca{
         int deltaX = Math.abs(getPosX(this.getPosicao()) - getPosX(destino));
         int deltaY = Math.abs(getPosY(this.getPosicao()) - getPosY(destino));
 
-        if(deltaX > 1 || deltaY > 1) // Fora do alcance do Rei
-            return false;
-
         int nx = getPosX(destino);
         int ny = getPosY(destino);
         if(nx < 0 || nx >= 8 || ny < 0 || ny >= 8) // Posição do grid inválida
             return false;
 
         if(this.getTabuleiro().getPeca(nx, ny) != null){ // Caso a posição não esteja vazia
-            if(this.getTabuleiro().getPeca(nx, ny).getCorDono() != this.getCorDono()){ // Peca inimiga
+            if(!this.getTabuleiro().getPeca(nx, ny).getCorDono().equals(this.getCorDono())){ // Peça inimiga
                 return this._make_move(destino);
             }
-            else return false;
+            else 
+                return false;
         }
         else { // Caso a posição esteja vazia
+            if(!this.getMovimentou() && deltaX == 2 && deltaY == 0) { // Implementação do roque
+                if(this.getCorDono().equals("branca")) {
+                    if(destino.equals("G1")) { // Roque menor do Rei das brancas
+                        if(this.getTabuleiro().getPeca("F1") == null && this.getTabuleiro().getPeca("G1") == null) { //Checa se não há nenhuma peça entre o Rei e a Torre
+                            if(this.getTabuleiro().getPeca("H1").getClass() == Torre.class && !this.getTabuleiro().getPeca("H1").getMovimentou()) {
+                                this.getTabuleiro().getPeca("H1").moverPeca("F1");
+                                return this._make_move(destino);
+                            }
+                        }
+                    }
+                    else if(destino.equals("C1")) { // Roque maior do Rei das brancas
+                        if(this.getTabuleiro().getPeca("B1") == null && this.getTabuleiro().getPeca("C1") == null && this.getTabuleiro().getPeca("D1") == null) { //Checa se não há nenhuma peça entre o Rei e a Torre
+                            if(this.getTabuleiro().getPeca("A1").getClass() == Torre.class && !this.getTabuleiro().getPeca("A1").getMovimentou()) {
+                                this.getTabuleiro().getPeca("A1").moverPeca("D1");
+                                return this._make_move(destino);
+                            }
+                        }
+                    }
+                }
+                else if(this.getCorDono().equals("preta")) {
+                    if(destino.equals("G8")) { // Roque menor do Rei das pretas
+                        if(this.getTabuleiro().getPeca("F8") == null && this.getTabuleiro().getPeca("G8") == null) { //Checa se não há nenhuma peça entre o Rei e a Torre
+                            if(this.getTabuleiro().getPeca("H8").getClass() == Torre.class && !this.getTabuleiro().getPeca("H8").getMovimentou()) {
+                                this.getTabuleiro().getPeca("H8").moverPeca("F8");
+                                return this._make_move(destino);
+                            }
+                        }
+                    }
+                    else if(destino.equals("C8")){ // Roque maior do Rei das pretas
+                        if(this.getTabuleiro().getPeca("B8") == null && this.getTabuleiro().getPeca("C8") == null && this.getTabuleiro().getPeca("D8") == null) { //Checa se não há nenhuma peça entre o Rei e a Torre
+                            if(this.getTabuleiro().getPeca("A8").getClass() == Torre.class && !this.getTabuleiro().getPeca("A8").getMovimentou()) {
+                                this.getTabuleiro().getPeca("A8").moverPeca("D8");
+                                return this._make_move(destino);
+                            }
+                        }
+                    }
+                }
+                return false;
+            }
+            else if(deltaX > 1 || deltaY > 1) // Fora do alcance do Rei
+                return false;
             return this._make_move(destino);
         }
     }
---
+++ Torre.java
@@ -0,0 +1,65 @@
+public class Torre extends Peca{
+    public boolean moverPeca(String destino) {
+        // Implementação do movimento da Torre
+        int deltaX = Math.abs(getPosX(this.getPosicao()) - getPosX(destino));
+        int deltaY = Math.abs(getPosY(this.getPosicao()) - getPosY(destino));
+
+        if(deltaX > 0 && deltaY > 0) // Fora do alcance da Torre
+            return false;
+
+        int nx = getPosX(destino);
+        int ny = getPosY(destino);
+        if(nx < 0 || nx >= 8 || ny < 0 || ny >= 8) // Posição do grid inválida
+            return false;
+
+        if(deltaX == 0){
+            if(ny > getPosY(this.getPosicao())) { // Caso a Torre esteja se movendo para cima
+                for(int i = getPosY(this.getPosicao()) + 1; i < ny; i++) {
+                    if(this.getTabuleiro().getPeca(getPosX(this.getPosicao()), i) != null) { // Caso haja alguma peça entre a Torre e o destino
+                        return false;
+                    }
+                }
+            }
+            else { // Caso a Torre esteja se movendo para baixo
+                for(int i = getPosY(this.getPosicao()) - 1; i > ny; i--) {
+                    if(this.getTabuleiro().getPeca(getPosX(this.getPosicao()), i) != null) { // Caso haja alguma peça entre a Torre e o destino
+                        return false;
+                    }
+                }
+            }
+        }
+        if(deltaY == 0) {
+            if (nx > getPosX(this.getPosicao())) { // Caso a Torre esteja se movendo para a direita
+                for(int i = getPosX(this.getPosicao()) + 1; i < nx; i++) {
+                    if(this.getTabuleiro().getPeca(i, getPosY(this.getPosicao())) != null) // Caso haja alguma peça entre a Torre e o destino
+                        return false;
+                }
+            }
+            else { // Caso a Torre esteja se movendo para a esquerda
+                for(int i = getPosX(this.getPosicao()) - 1; i > nx; i--) {
+                    if(this.getTabuleiro().getPeca(i, getPosY(this.getPosicao())) != null) // Caso haja alguma peça entre a Torre e o destino
+                        return false;
+                }
+            }
+        }
+
+        if(this.getTabuleiro().getPeca(nx, ny) != null) { // Caso a posição de destino não esteja vazia
+            if(!this.getTabuleiro().getPeca(nx, ny).getCorDono().equals(this.getCorDono())) { // Peça inimiga
+                return this._make_move(destino);
+            }
+            else
+                return false;
+        }
+        else { // Caso a posição de destino esteja vazia
+            return this._make_move(destino);
+        }
+    }
+
+    public Torre(String corDono, Tabuleiro tabuleiro, String posicao){
+        super(corDono, tabuleiro, posicao);
+        if(corDono.equals("branca"))
+            setLabel("R");
+        else
+            setLabel("r");
+    }
+}
