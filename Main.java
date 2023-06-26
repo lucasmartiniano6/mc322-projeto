69e0ebd0fada72845dc0210f83c77cb9fc7d43a3
Lucas Martiniano <l231036@dac.unicamp.br>
2023-06-26 18:58:50 -0300

    Ajustar comentarios

--- Main.java
+++ Main.java
@@ -12,25 +12,5 @@ public class Main {
         // tabuleiro.setRelogio_pretas(relogio_pretas);
         // JanelaChess janela = new JanelaChess(tabuleiro);
         // janela.getWindow().repaint();
-        
-
-
-        // Relogio relogio1 = new Relogio(1, "brancas", janela);
-        // relogio1.startRelogio();
-
-        
-        // Scanner entrada = new Scanner(System.in);
-
-        // printarTabuleiro(tabuleiro); // Debugging do tabuleiro
-        // tabuleiro.saveBoard("fen/board.fen");
-
-        // // Debugging movimentos
-        // tabuleiro.setBoardFromFEN("fen/board.fen");
-        // while(true) {
-        //    String s1 = entrada.nextLine();
-        //    String s2 = entrada.nextLine();
-        //    tabuleiro.mover(s1, s2);
-           
-        //    printarTabuleiro(tabuleiro);
     }
 }
